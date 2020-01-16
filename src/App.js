8a793644664c4e8c9a45f3adba526af600731389
= <siheonl@andrew.cmu.edu>
2020-01-16 11:34:54 -0500

    router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Nav from "./components/Nav";
+import SearchPage from './SearchPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MovieDetail from './components/MovieDetail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div className='app'>
+          <Nav />
+          <Switch>
+            <Route path='/' exact component={SearchPage} />
+            <Route path='/movies/:id' component={MovieDetail} />
+          </Switch>
+        </div>
+      </Router>
+        
+      
+    )
+  }
 }
 
 export default App;
